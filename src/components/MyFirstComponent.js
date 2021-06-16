c0203de97efdd09ef20a376da864eeff296aaea8
René González <rdgonzu@gmail.com>
2021-06-15 22:39:55 -0400

    Destructuring Component. Example of React.Fragment and loop in JSX.

--- src/components/MyFirstComponent.js
+++ src/components/MyFirstComponent.js
@@ -1,9 +1,32 @@
-import React from "react";
-
-class MyFirstComponent extends React.Component {
+import React, {Component} from "react";
+class MyFirstComponent extends Component {
     render () {
+
+        let cities = ['Santiago', 'Valparaíso', 'La Serena'];
+
         return (
-            <h2>This is a text from My First Component</h2>
+            <React.Fragment>
+
+                <h2>This is a text from My First Component</h2>
+                <h2>This is another text from My First Component</h2>
+
+                <h3>City list (loop example):</h3>
+
+                <ul>
+                    {
+                        cities.map((city, i) => {
+
+                            console.log('City: ', city);
+
+                            return (
+                                <li key={i}>{city}</li>
+                            )
+
+                        })
+                    }
+                </ul>
+
+            </React.Fragment>
         );
     }
 }
