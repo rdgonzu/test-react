1cd4bbe3b91fd3ffb87ec987f0f849a67d0c9aa4
René González <rdgonzu@gmail.com>
2021-06-24 23:31:20 -0400

    Adding page "404".

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import TestsComponent from './components/TestsComponent';
 import MyFirstComponent from './components/MyFirstComponent';
 import Movies from './components/Movies';
+import Error from "./components/Error";
 
 class Router extends Component {
     render () {
@@ -15,6 +16,8 @@ class Router extends Component {
                     <Route exact path="/my-first-component" component={MyFirstComponent}></Route>
                     
                     <Route exact path="/tests-component" component={TestsComponent}></Route>
+
+                    <Route component={Error}></Route>
 
                 </Switch>
             </BrowserRouter>
---
+++ src/components/Error.js
@@ -0,0 +1,7 @@
+const Error = () => {
+    return (
+        <h2>Page not found</h2>
+    );
+}
+
+export default Error;
